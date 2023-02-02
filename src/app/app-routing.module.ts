6f049fc0ba3988faa12ebd51adc7645db3357e4b
Sumit Sinha <sumitsix42@gmail.com>
2023-02-02 14:43:04 +0530

    Add files via upload
    brands module created.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RollsroyceComponent } from './brands/rollsroyce/rollsroyce.component';
+
+const routes: Routes = [ {path: '', component:RollsroyceComponent},
+  { path: 'brands', loadChildren: () => import('./brands/brands.module').then(m => m.BrandsModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
