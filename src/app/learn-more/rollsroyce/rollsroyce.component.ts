33454bebe56f2603374ae933c950255db629523b
Shant1610 <116144141+Shant1610@users.noreply.github.com>
2023-02-02 20:21:42 +0530

    Add files via upload
    learnmore module created

---
+++ src/app/learn-more/rollsroyce/rollsroyce.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rollsroyce',
+  templateUrl: './rollsroyce.component.html',
+  styleUrls: ['./rollsroyce.component.scss']
+})
+export class RollsroyceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  toOverview(){
+    document.getElementById("sec2")?.scrollIntoView({behavior:"smooth"});
+  }
+  toKey()
+  {
+  document.getElementById("sec3")?.scrollIntoView({behavior:"smooth"});
+  }
+  toSummary(){
+    document.getElementById("sec4")?.scrollIntoView({behavior:"smooth"});
+  }
+  toColours(){
+    document.getElementById("sec5")?.scrollIntoView({behavior:"smooth"});
+  }
+  toMileage(){
+    document.getElementById("sec6")?.scrollIntoView({behavior:"smooth"});
+  }
+}
