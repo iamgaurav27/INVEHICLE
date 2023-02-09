19c40d2cabdb6f362bacb172a1f70c47f47131c5
iamgaurav27 <gk44805@gmail.com>
2023-02-09 14:38:10 +0530

    module removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    PageNotFoundComponent,
+   
     HomeComponent
 
   ],
