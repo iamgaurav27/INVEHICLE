1a2df189e11c2abda83d0f2a43693dad07cf8e76
sugale360 <109960372+sugale360@users.noreply.github.com>
2023-02-13 09:45:30 +0530

    added sagar-forms module with login and register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [ 
   // {path: '', component:RollsroyceComponent},
 {path:'',component:HomeComponent},
+  { path: 'sagar-forms', loadChildren: () => import('./sagar-forms/sagar-forms.module').then(m => m.SagarFormsModule) },
 
 {path:'**',component : PageNotFoundComponent}]
 ;
