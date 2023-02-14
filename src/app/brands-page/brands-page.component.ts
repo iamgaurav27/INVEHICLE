d7ccdd2fff278ea3df5945429fda0c9c086d6eee
Shant1610 <116144141+Shant1610@users.noreply.github.com>
2023-02-14 17:35:16 +0530

    Add files via upload

--- src/app/brands-page/brands-page.component.ts
+++ src/app/brands-page/brands-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BrandDetailsService } from '../services/brand-details.service';
+import { LearnMorepageService } from '../services/learn-morepage.service';
 
 @Component({
   selector: 'app-brands-page',
@@ -11,11 +12,13 @@ export class BrandsPageComponent implements OnInit {
   constructor(
     private param: ActivatedRoute,
     private service: BrandDetailsService,
+    private service1: LearnMorepageService,
     
   ) {}
 
   brandData: any;
   getBrandId: any;
+  lmData: any;
   ngOnInit(): void {
     this.getBrandId = this.param.snapshot.paramMap.get('id');
     if (this.getBrandId) {
@@ -23,5 +26,6 @@ export class BrandsPageComponent implements OnInit {
         return value.id == this.getBrandId;
       });
     }
+    this.lmData = this.service1.learnMoreDetails
   }
 }
